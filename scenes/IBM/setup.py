47c497a80b03917d2c1462d94210a6c89cf02ffe
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2018-11-28 17:35:31 +0100

    [SofaCaribou] cleanup

--- scenes/IBM/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('Helpers',
-                    sources = ['helpers.cpp'])
-
-setup (name = 'Helpers',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
