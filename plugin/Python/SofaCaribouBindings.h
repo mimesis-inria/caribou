11a233bbc394878fb1d35a3a873179515e30a7d9
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2018-11-28 17:34:30 +0100

    [SofaCaribou] cleanup

--- plugin/Python/SofaCaribouBindings.h
+++
@@ -1,9 +0,0 @@
-#ifndef CARIBOU_SOFABINDINGS_H
-#define CARIBOU_SOFABINDINGS_H
-
-#include <sofa/config.h>
-
-SOFA_EXPORT_DYNAMIC_LIBRARY void bindCaribouPythonModule();
-
-
-#endif //CARIBOU_SOFABINDINGS_H
