02ed46ef59e32c8cef4f9e1c06665588e1a6d357
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2019-09-23 13:27:30 +0200

    [SofaCaribou] Remove dependency to SofaPython

--- src/SofaCaribou/Python/SofaCaribou.cpp
+++ src/SofaCaribou/Python/SofaCaribou.cpp
@@ -3,9 +3,6 @@
 #include <pybind11/functional.h>
 #include <pybind11/eigen.h>
 
-#include <SofaPython3/bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_BaseObject.h>
-#include <SofaPython3/src/SofaPython3/PythonFactory.h>
-
 #include <SofaCaribou/GraphComponents/Topology/FictitiousGrid.h>
 #include <SofaCaribou/GraphComponents/Forcefield/HexahedronElasticForce.h>
 #include "HexahedronElasticForce.h"
@@ -18,11 +15,11 @@ PYBIND11_MODULE(SofaCaribouPython, m) {
     m.doc() = "SofaCaribou module";
 
     using FictitiousGrid2D = SofaCaribou::GraphComponents::topology::FictitiousGrid<sofa::defaulttype::Vec2Types>;
-    py::class_<FictitiousGrid2D, BaseObject, FictitiousGrid2D::SPtr> fictitious_grid_2d (m, "FictitiousGrid2D");
+    py::class_<FictitiousGrid2D, BaseObject, std::shared_ptr<FictitiousGrid2D>> fictitious_grid_2d (m, "FictitiousGrid2D");
     fictitious_grid_2d.def("set_implicit_test_function", &FictitiousGrid2D::set_implicit_test_function);
 
     using FictitiousGrid3D = SofaCaribou::GraphComponents::topology::FictitiousGrid<sofa::defaulttype::Vec3Types>;
-    py::class_<FictitiousGrid3D, BaseObject, FictitiousGrid3D::SPtr> fictitious_grid (m, "FictitiousGrid");
+    py::class_<FictitiousGrid3D, BaseObject, std::shared_ptr<FictitiousGrid3D>> fictitious_grid (m, "FictitiousGrid");
     fictitious_grid.def("set_implicit_test_function", &FictitiousGrid3D::set_implicit_test_function);
 
     SofaCaribou::Python::addHexahedronElasticForce(m);
