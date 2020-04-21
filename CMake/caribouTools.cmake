643ef002f4afc6e7c5549248f87daf6ae09d8c51
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2020-04-21 17:37:41 +0200

    [Caribou] CMake cleanups

--- CMake/caribouTools.cmake
+++ CMake/caribouTools.cmake
@@ -1,25 +1,38 @@
+function(dump_cmake_variables)
+    get_cmake_property(_variableNames VARIABLES)
+    list (SORT _variableNames)
+    foreach (_variableName ${_variableNames})
+        if (ARGV0)
+            unset(MATCHED)
+            string(REGEX MATCH ${ARGV0} MATCHED ${_variableName})
+            if (NOT MATCHED)
+                continue()
+            endif()
+        endif()
+        message(STATUS "${_variableName}=${${_variableName}}")
+    endforeach()
+endfunction()
+
 function(caribou_add_python_module NAME)
     set(options QUIET)
-    set(oneValueArgs PREFIX TESTS_PREFIX DESTINATION TESTS_DESTINATION PYTHON_VERSION TARGET_NAME)
+    set(oneValueArgs PREFIX TESTS_PREFIX DESTINATION TESTS_DESTINATION TARGET_NAME)
     set(multiValueArgs SOURCE_FILES PYTHON_FILES PYTHON_TEST_FILES DEPENDS)
 
     cmake_parse_arguments(A "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    if (NOT DEFINED A_PYTHON_VERSION)
-        message(FATAL_ERROR "A python version must be provided.")
-    endif()
+    find_package(pybind11 QUIET CONFIG REQUIRED)
 
     if (A_TARGET_NAME)
         set(TARGET_NAME ${A_TARGET_NAME})
     else()
-        set(TARGET_NAME "${NAME}.python.${A_PYTHON_VERSION}")
+        set(TARGET_NAME "${NAME}.python.${PYTHON_LIBRARY_SUFFIX}")
     endif()
 
 
     if (A_TESTS_PREFIX)
         set(TESTS_PREFIX "${A_TESTS_PREFIX}")
     else()
-        set(TESTS_PREFIX "bin/python_tests/python${A_PYTHON_VERSION}")
+        set(TESTS_PREFIX "bin/python_tests/python${PYTHON_LIBRARY_SUFFIX}")
     endif()
 
     if (A_DESTINATION)
@@ -35,55 +48,32 @@ function(caribou_add_python_module NAME)
     endif ()
 
     if (A_SOURCE_FILES)
-        set(PYBIND11_PYTHON_VERSION ${A_PYTHON_VERSION})
         set(PYBIND11_CPP_STANDARD -std=c++17)
-        unset(PYTHON_MODULE_PREFIX)
-        unset(PYTHON_MODULE_EXTENSION)
-        unset(PYTHON_EXECUTABLE)
-        unset(PYTHONLIBS_FOUND)
-        unset(PYTHON_MODULE_EXTENSION)
-        unset(PythonLibsNew_FIND_REQUIRED )
 
         project(${TARGET_NAME})
-
-        if (${A_PYTHON_VERSION} VERSION_LESS "3.0")
-            find_package(PythonLibs ${A_PYTHON_VERSION} QUIET)
-        else()
-            find_package(Python3 QUIET COMPONENTS Development)
-            set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE})
-        endif ()
 
         if (A_PREFIX)
             set(PREFIX "${A_PREFIX}")
         else()
-            if ("${Python3_VERSION_MINOR}" STREQUAL "0")
-                set(PREFIX "python${Python3_VERSION_MAJOR}/site-packages")
-            else()
-                set(PREFIX "python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}/site-packages")
-            endif()
+            set(PREFIX "python${PYTHON_LIBRARY_SUFFIX}/site-packages")
         endif()
-
-        find_package(pybind11 CONFIG REQUIRED)
 
         set(MODULENAME ${NAME})
 
         if (NOT A_QUIET)
-            message(STATUS "${NAME} with python ${A_PYTHON_VERSION} support (python version ${PYTHON_VERSION_STRING}, pybind11 version ${pybind11_VERSION})")
+            message(STATUS "${NAME} with python ${PYTHON_VERSION_STRING} support (pybind11 version ${pybind11_VERSION})")
         endif ()
 
         pybind11_add_module(${TARGET_NAME} SHARED "${A_SOURCE_FILES}")
+        target_link_libraries(${TARGET_NAME} PUBLIC ${A_DEPENDS} pybind11::module)
+        target_include_directories(${TARGET_NAME} PUBLIC $<INSTALL_INTERFACE:include>)
 
-        target_link_libraries(${TARGET_NAME} PUBLIC ${A_DEPENDS} ${PYTHON_LIBRARIES} pybind11::module)
-
-        target_include_directories(${TARGET_NAME}
-                                   PUBLIC $<INSTALL_INTERFACE:include>
-                                   )
         if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
             target_compile_options(${TARGET_NAME} PRIVATE -fsized-deallocation)
         endif()
 
         target_compile_options(${TARGET_NAME} PRIVATE -Dregister=)
-        set_property(TARGET ${TARGET_NAME} PROPERTY CXX_STANDARD 17)
+        target_compile_features(${PROJECT_NAME} PUBLIC cxx_std_17)
 
         set_target_properties(
             ${TARGET_NAME}
