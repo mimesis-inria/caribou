eae0250d5b43e984546a21551ec186944067f0fe
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2020-05-29 10:45:00 +0200

    [SofaCaribou] Fix python bindings

--- src/SofaCaribou/Python/Topology/FictitiousGrid.cpp
+++ src/SofaCaribou/Python/Topology/FictitiousGrid.cpp
@@ -1,6 +1,9 @@
 #include <SofaCaribou/Python/Topology/FictitiousGrid.h>
 #include <SofaCaribou/Topology/FictitiousGrid.inl>
 
+#include <SofaPython3/PythonFactory.h>
+#include <SofaPython3/Sofa/Core/Binding_BaseObject.h>
+
 #include <pybind11/stl.h>
 
 namespace SofaCaribou::topology::python {
@@ -11,11 +14,16 @@ using sofa::defaulttype::Vec3Types;
 template <typename DataTypes>
 auto create_fictitious_grid(py::module & m) {
     std::string name = "FictitiousGrid" + std::string(DataTypes::Name());
-    py::class_<FictitiousGrid<DataTypes>, std::shared_ptr<FictitiousGrid<DataTypes>>> c (m, name.c_str());
+    py::class_<FictitiousGrid<DataTypes>, sofa::core::objectmodel::BaseObject, sofa::core::sptr<FictitiousGrid<DataTypes>>> c (m, name.c_str());
     c.def("number_of_cells", &FictitiousGrid<DataTypes>::number_of_cells);
     c.def("number_of_nodes", &FictitiousGrid<DataTypes>::number_of_nodes);
     c.def("number_of_subdivisions", &FictitiousGrid<DataTypes>::number_of_subdivisions);
     c.def("cell_volume_ratio_distribution", &FictitiousGrid<DataTypes>::cell_volume_ratio_distribution);
+
+    sofapython3::PythonFactory::registerType<FictitiousGrid<DataTypes>>([](sofa::core::objectmodel::Base* o) {
+        return py::cast(dynamic_cast<FictitiousGrid<DataTypes>*>(o));
+    });
+
     return c;
 }
 
