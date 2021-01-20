e24bcf280f649885f9372e3d5914ec51a6e50fbd
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2021-01-20 13:32:54 +0100

    [SofaCaribou] Import SOFA's python bindings before SofaCaribou

--- src/SofaCaribou/Python/__init__.py
+++ src/SofaCaribou/Python/__init__.py
@@ -1,1 +1,6 @@
-from .${MODULENAME}  import *
+# We first import Sofa.Core since without this, bindings will be unable
+# to have sofa::core::objectmodel::BaseObject as a base class
+from Sofa import Core
+
+# Then, we import the actual bindings
+from .${MODULENAME} import *
