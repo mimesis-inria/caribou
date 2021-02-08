5efa7500c06204aceeea9c12b8eabbe418dcabb4
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2021-02-08 14:03:31 +0100

    [SofaCaribou] Multiple materials example scene (part 2)

--- scenes/multiple_materials.py
+++ scenes/multiple_materials.py
@@ -18,22 +18,40 @@ def createScene(root):
     meca = root.addChild("mechanical")
     meca.addObject('StaticODESolver', newton_iterations=10, printLog=True)
     meca.addObject('LDLTSolver', backend="Pardiso")
-    meca.addObject('MechanicalObject', src='@../grid')
+    meca.addObject('MechanicalObject', name='mo', src='@../grid')
 
-    # Complete hexa container (only needed for the BoxROI later on, a bug which should be fixed...)
+    # Complete hexa container (only needed for the BC BoxROI later on, a bug from SOFA's side)
     meca.addObject('HexahedronSetTopologyContainer', src='@../grid', name='mechanical_topology')
 
-    # Material 1 (first half of the beam)
-    meca.addObject('BoxROI', name='material_1_box', hexahedra='@../grid.hexahedra', box=[-7.5, -7.5, -0.9, 7.5, 7.5, 40.1])
-    meca.addObject('HexahedronSetTopologyContainer', hexahedra='@material_1_box.hexahedraInROI', name='material_1_topology')
-    meca.addObject('SaintVenantKirchhoffMaterial', young_modulus=300000, poisson_ratio=0.499, name='material_1')
-    meca.addObject('HyperelasticForcefield', topology="@material_1_topology", material='@material_1')
+    #######################################
+    # Material 1 (first half of the beam) #
+    #######################################
 
+    # - Mechanics
+    meca.addObject('BoxROI', name='material_1_box', hexahedra='@../grid.hexahedra', box=[-7.5, -7.5, -0.9, 7.5, 7.5, 40.1])
+    meca.addObject('HexahedronSetTopologyContainer', hexahedra='@material_1_box.hexahedraInROI', name='material_1_hexahedrons')
+    meca.addObject('SaintVenantKirchhoffMaterial', young_modulus=300000, poisson_ratio=0.499, name='material_1')
+    meca.addObject('HyperelasticForcefield', topology="@material_1_hexahedrons", material='@material_1')
+    # - Visual
+    meca.addObject('QuadSetTopologyContainer', name='material_1_quads')
+    meca.addObject('Hexa2QuadTopologicalMapping', input='@material_1_hexahedrons', output='@material_1_quads')
+    meca.addObject('QuadSetTopologyModifier')
+    meca.addObject('OglModel', position='@mo.position', quads='@material_1_quads.quads', color='red')
+
+    #######################################
     # Material 2 (second half of the beam)
-    meca.addObject('BoxROI', name='material_2_box', hexahedra='@../grid.hexahedra', box=[-7.5, -7.5, -30.9, 7.5, 7.5, 80.1])
-    meca.addObject('HexahedronSetTopologyContainer', hexahedra='@material_2_box.hexahedraInROI', name='material_2_topology')
+    #######################################
+
+    # - Mechanics
+    meca.addObject('BoxROI', name='material_2_box', hexahedra='@../grid.hexahedra', box=[-7.5, -7.5, 30.9, 7.5, 7.5, 80.1])
+    meca.addObject('HexahedronSetTopologyContainer', hexahedra='@material_2_box.hexahedraInROI', name='material_2_hexahedrons')
     meca.addObject('SaintVenantKirchhoffMaterial', young_modulus=3000, poisson_ratio=0.3, name='material_2')
-    meca.addObject('HyperelasticForcefield', topology="@material_2_topology", material='@material_2')
+    meca.addObject('HyperelasticForcefield', topology="@material_2_hexahedrons", material='@material_2')
+    # - Visual
+    meca.addObject('QuadSetTopologyContainer', name='material_2_quads')
+    meca.addObject('Hexa2QuadTopologicalMapping', input='@material_2_hexahedrons', output='@material_2_quads')
+    meca.addObject('QuadSetTopologyModifier')
+    meca.addObject('OglModel', position='@mo.position', quads='@material_2_quads.quads', color='blue')
 
     # Fix the left side of the beam
     meca.addObject('BoxROI', name='fixed_roi', quad='@../grid.quad', box=[-7.5, -7.5, -0.9, 7.5, 7.5, 0.1])
