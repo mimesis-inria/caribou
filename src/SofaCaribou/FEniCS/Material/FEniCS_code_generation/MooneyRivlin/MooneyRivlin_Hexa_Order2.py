a961746cfbf3cdae5c7b52f1c6f70beb6a3c7109
Arnaud <mazier-stg@anatoscope.com>
2022-04-08 18:19:24 +0200

    add Mooney-Rivlin again OO

---
+++ src/SofaCaribou/FEniCS/Material/FEniCS_code_generation/MooneyRivlin/MooneyRivlin_Hexa_Order2.py
@@ -0,0 +1,45 @@
+from ufl import (Coefficient, Constant, Identity,
+                 TestFunction, TrialFunction, det,
+                 VectorElement, derivative, dx, grad,
+                 hexahedron, tr, variable, ln)
+
+# Function spaces
+cell = hexahedron
+d = cell.geometric_dimension()
+element = VectorElement("S", cell, 2)
+
+# Trial and test functions
+du = TrialFunction(element)  # Incremental displacement
+v = TestFunction(element)  # Test function
+
+# Functions
+u = Coefficient(element)  # Displacement from previous iteration
+
+# Kinematics
+I = Identity(d)  # Identity tensor
+F = variable(I + grad(u))  # Deformation gradient
+C = variable(F.T * F)  # Right Cauchy-Green tensor
+I_C = tr(C)
+II_C = (1.0/2.0)*(tr(C)**2 - tr(C*C))
+III_C = det(C)
+J = III_C**(1.0/2.0)
+
+# Elasticity parameters
+C_01 = Constant(cell)
+C_10 = Constant(cell)
+K = Constant(cell)
+
+# stored strain energy density (nearly incompressible Mooney-Rivlin model)
+psi = Constant(C_01)*(J**(-2.0/3.0)*I_C - 3) + Constant(C_10)*(J**(-4.0/3.0)*II_C - 3) + 0.5 * K * (ln(J))**2
+
+# Total potential energy
+Pi = psi * dx(degree=2)
+
+# First variation of Pi (directional derivative about u in the direction of v)
+F = derivative(Pi, u, v)
+
+# Compute Jacobian of F
+J = derivative(F, u, du)
+
+# Export forms
+forms = [F, J, Pi]
